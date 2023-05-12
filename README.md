71021e920a9ac8d53e4bf49ac404e5077f39f9b2
kikimora_ <crull2022@gmail.com>
2023-05-12 16:37:31 +0200

    1.1.0

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-sorting
+<h2 align="center"> sorting </h2>
+
+![intro](video/intro.gif)
+
+
+- [Quick start](#quick)
+- [What can do?](#what)
+- [New](#new)
+- [Plan](#plan)
+
+
+<h4 id="quick">Quick start</h4>
+
+just download the release version of the system you want
+
+**❗ In this version it must be run in the directory where it lies ❗**
+
+<h4 id="what">What can do?</h4>
+
+this sorter can sort files by category, and if there is a recurring file,<br>
+it will add its number to it in parentheses
+
+![sample](video/sample.gif)
+
+You can configure it in the **config.toml** file 
+
+What is before = is the name of the directory, and after it in [...], they should be in lowercase without a dot ( . )<br> 
+and in single brackets ( ' ) and between them there should be a coma ( , ).
+
+```toml
+Word = ['docx', 'doc', 'odt', 'rtf', 'txt']
+```
+
+<h4 id="new">New</h4>
+
+
+
+<h4 id="plan">Plan</h4>
+
+- [x] 1.1.0 - add multiple files (this is when several files have the same name
+    <br> and not to overwrite the file just add the number at the end as Texte(number).txt)
+
+- [ ] 1.2.0 - add crossplatform for Winduos
+
+- [ ] 1.3.0 - add background mode to work when something is changed in the directory
+
+- [ ] 1.4.0 - add it to be able to sort files where it's needed and it doesn't matter where it is
+
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use std::fs::{self, ReadDir};
 use std::path::Path;
 use toml::Value;
+use std::error;
 
 
 fn create_dir(name_dir: &str) -> std::io::Result<()> { 
 
     let dir_list = fs::read_dir("..")?; 
 
-    for file_or_dir in dir_list {
+    for file_or_dir in dir_list { 
         let file_or_dir_str: String = file_or_dir?.file_name().to_string_lossy().to_string();
         let file_or_dir_str_metadata = fs::metadata(format!("../{}", &file_or_dir_str))?;
 
@@ -23,25 +24,53 @@ fn create_dir(name_dir: &str) -> std::io::Result<()> {
 
 
 fn file_transfer(name_file: &str, name_dir: &str) -> std::io::Result<()> {
-    let _str = format!("../{name_file}");
+    let _str = format!("../{}", &name_file);
     let name_file_pach = Path::new(&_str);
 
     let _str = format!("../{}/{}", name_dir, name_file);
     let dir_and_file_pach = Path::new(&_str);
+
+    let (file, exc) = name_file.split_once(".").unwrap();
+    let strg = format!("../{}/{}(1).{}", &name_dir, file, exc);
+    let path_dir_file_rex = Path::new(&strg);
+
+
     
-    if name_file_pach.exists() {
-        fs::copy(&name_file_pach, dir_and_file_pach)?;
-        fs::remove_file(name_file_pach)?;
+    if name_file_pach.exists() && !dir_and_file_pach.exists(){
+        fs::copy(&name_file_pach, &dir_and_file_pach)?;
+        fs::remove_file(&name_file_pach)?;
         return Ok(());
+
+    } else if name_file_pach.exists() && dir_and_file_pach.exists() && !path_dir_file_rex.exists() { 
+        fs::copy(&name_file_pach, &path_dir_file_rex)?;
+        fs::remove_file(&name_file_pach)?;
+        return Ok(());
+
+    } else if name_file_pach.exists() && dir_and_file_pach.exists() && path_dir_file_rex.exists() {
+        let (file_name, exc) = &name_file.split_once(".").unwrap();
+        let mut file_name_index: u16 = 1;
+
+        let new_file = loop {
+            file_name_index += 1;
+            let strg = format!("../{}/{}({}).{}", &name_dir, &file_name, &file_name_index, &exc);
+            let file_clon = Path::new(&strg);
+
+            if !file_clon.exists() {
+               break strg; 
+            }
+        };
+        fs::copy(&name_file_pach, new_file)?;
+        fs::remove_file(&name_file_pach)?;
+        return Ok(()); 
     }
 
     Ok(())
 }
 
 
-fn conf_read_toml(path_file: &Path) -> std::io::Result<Value>{
+fn conf_read_toml(path_file: &Path) -> Result<Value, Box<dyn error::Error>>{
     let conf = std::fs::read_to_string(path_file)?; 
-    let toml: Value = toml::from_str(&conf).unwrap();
+    let toml: Value = toml::from_str(&conf)?;
     Ok(toml)
 }
     
@@ -58,9 +87,9 @@ fn sord(list_dir: ReadDir, config_path: Value) -> std::io::Result<()> {
                 if file_path.exists() {
                     if let Some(file_exc) = file_path.extension() {
 
-                        if file_path.metadata().expect("120").is_file() {
+                        if file_path.metadata().unwrap().is_file() {
 
-                                if file_exc.to_str().unwrap() == exc.as_str().expect("102") {
+                                if file_exc.to_str().unwrap() == exc.as_str().unwrap() {
                                     let file = file_path.file_name().unwrap().to_str().unwrap();
 
                                     create_dir(&key)?;
@@ -76,8 +105,12 @@ fn sord(list_dir: ReadDir, config_path: Value) -> std::io::Result<()> {
 }
 
 fn main() {    
-    let config = Path::new("./config.toml"); 
-    let config_path = conf_read_toml(config).expect("\nERROR \nThere is no configuration file\n\n");
+    let config = Path::new("config.toml"); 
+    let config_path = conf_read_toml(config).unwrap_or_else(|err| {
+        eprintln!("Failed to read file contents: {}", err);
+        std::process::exit(1);
+    });
+    
     let list_dir = fs::read_dir("..").unwrap();
 
     sord(list_dir, config_path).unwrap();
